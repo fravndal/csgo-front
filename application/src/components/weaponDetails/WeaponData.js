62fcef66be4bc841c45acb89edea6ec345c0d0ce
ravndalf <fredrik.ravndal@allegro.no>
2020-02-07 10:01:08 +0100

    css on single weapon

--- application/src/components/weaponDetails/WeaponData.js
+++ application/src/components/weaponDetails/WeaponData.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaChevronLeft } from "react-icons/fa";
 
 const WeaponData = ({ weapon }) => {
   return (
@@ -6,15 +8,23 @@ const WeaponData = ({ weapon }) => {
       <div className="weapon-frame container">
         <div className="row">
           <div className="col-3">
+            <Link to="/weapons">
+              <FaChevronLeft color="white" />
+
+              <span className="backspace">Go back</span>
+            </Link>
+          </div>
+        </div>
+        <div className="row">
+          <div className="weapon-image-frame col-3">
             <img
               src={`https://csgo-weapons.s3.eu-north-1.amazonaws.com/${weapon.weaponImageName}`}
               alt={weapon.weaponImageName}
-              className="weapon-image-standard rounded float-left"
+              className="weapon-image-standard "
             />
           </div>
-          <div className="col-9">
-            Name:
-            {weapon.weaponName}
+          <div className="col-8 weapon-name-row">
+            <span className="weapon-name">{weapon.weaponName}</span>
           </div>
 
           {weapon.weaponType}
@@ -28,6 +38,17 @@ const WeaponData = ({ weapon }) => {
           {weapon.penetrationPower}
           {weapon.ammoReserve}
           {weapon.bulletRange}
+        </div>
+        <div>
+          <div>
+            <span>
+              {(
+                (weapon.fireRate / 60) *
+                weapon.damage *
+                weapon.bullets
+              ).toFixed(2)}
+            </span>
+          </div>
         </div>
       </div>
     </div>
