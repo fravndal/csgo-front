fc87ed18d76eac64b05144c77c64ffef8424d2ae
ravndalf <fredrik.ravndal@allegro.no>
2020-02-07 14:53:36 +0100

    css work

--- application/src/components/weaponDetails/WeaponData.js
+++ application/src/components/weaponDetails/WeaponData.js
@@ -7,7 +7,7 @@ const WeaponData = ({ weapon }) => {
     <div className="outer-frame">
       <div className="weapon-frame container">
         <div className="row">
-          <div className="col-3">
+          <div className="backspace-frame col-3">
             <Link to="/weapons">
               <FaChevronLeft color="white" />
 
@@ -16,39 +16,56 @@ const WeaponData = ({ weapon }) => {
           </div>
         </div>
         <div className="row">
-          <div className="weapon-image-frame col-3">
+          <div className="weapon-image-frame col-4 rowSpan-7">
             <img
               src={`https://csgo-weapons.s3.eu-north-1.amazonaws.com/${weapon.weaponImageName}`}
               alt={weapon.weaponImageName}
-              className="weapon-image-standard "
+              className="weapon-image-standard"
             />
           </div>
-          <div className="col-8 weapon-name-row">
-            <span className="weapon-name">{weapon.weaponName}</span>
+          <div className="weapon-main-details col-7">
+            <div className="weapon-name-row">
+              <span className="weapon-name">{weapon.weaponName}</span>
+            </div>
+            <div className="weapon-type-row">
+              <span className="weapon-type">Type: {weapon.weaponType}</span>
+            </div>
+            <div className="weapon-price-row">
+              <span className="weapon-price">Price: {weapon.price}</span>
+            </div>
+            <div className="weapon-kill-award-row">
+              <span className="weapon-kill-award">
+                Kill award: {weapon.killAward}
+              </span>
+            </div>
+            <div className="weapon-damage-row">
+              <span className="weapon-damage">Damage: {weapon.damage}</span>
+            </div>
+            <div className="weapon-magazine-size-row">
+              <span className="weapon-magazine-size">
+                Magazine size: {weapon.magazineSize}
+              </span>
+            </div>
+            <div className="weapon-dps-row">
+              <span className="weapon-dps">
+                Damage per second:{" "}
+                {(
+                  (weapon.fireRate / 60) *
+                  weapon.damage *
+                  weapon.bullets
+                ).toFixed(2)}
+              </span>
+            </div>
           </div>
-
-          {weapon.weaponType}
-          {weapon.price}
-          {weapon.killAward}
-          {weapon.damage}
-          {weapon.magazineSize}
+        </div>
+        <div>
+          <span></span>
           {weapon.bullets}
           {weapon.armorPenetration}
           {weapon.fireRate}
           {weapon.penetrationPower}
           {weapon.ammoReserve}
           {weapon.bulletRange}
-        </div>
-        <div>
-          <div>
-            <span>
-              {(
-                (weapon.fireRate / 60) *
-                weapon.damage *
-                weapon.bullets
-              ).toFixed(2)}
-            </span>
-          </div>
         </div>
       </div>
     </div>
